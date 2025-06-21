779f2c23f4fbc569a6b1ff185cb08a9bc2a302e3
Joseph <mwanikij118@gmail.com>
2025-06-21 20:56:49 +0300

    Adds index,js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    const button = document.getElementById('button');
+    
+    button.addEventListener('click', function() {
+        alert(input.value);
+    });
 }
